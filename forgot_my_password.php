37c4556b37226ae8a7a7d99d878dccddfedfd1af
Mateusz <gorzanski.mateusz@gmail.com>
2017-04-11 11:03:29 +0200

    Dodano formularz akcji zresetowania hasła

--- forgot_my_password.php
+++ forgot_my_password.php
@@ -17,9 +17,30 @@ head();
 
     <?php
     if(!empty($_GET['action']) && $_GET['action'] == 'reset_password'):
-        if(!empty($_GET['email']) && !empty($_GET['session_id'])):
-        
-        endif;
+        if(!empty($_GET['email']) && !empty($_GET['session_id'])): ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Zresetuj hasło</h3>
+            </div>
+            <div class="panel-body">
+                <p>Użyj poniższego formularza, aby zmienić hasło do Twojego konta.</p>
+                <form action="" method="post" class="form-horizontal">
+                    <?php if(!empty($user->reset_password_statement) && $user->reset_password_statement=='wrong_email'): ?>
+                    <div class="form-group has-error">
+                    <?php else: ?>
+                    <div class="form-group">
+                    <?php endif; ?>
+                        <label for="inputResetEmail">Nowe hasło</label>
+                        <input type="email" class="form-control" id="inputResetEmail" name="reset_email" value="<?php if(!empty($_POST['user_name'])):echo $_POST['user_name'];endif; ?>" size="35" />
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Wyślij</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <?php else: header('Location: index.php'); ?>
+        <?php endif;
     else: ?>
     <?php if(!empty($user->reset_password_statement) && $user->reset_password_statement=='wrong_email'): ?>
     <div class="alert alert-danger" role="alert">
